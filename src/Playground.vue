4454adb4d646330eab932a30f5924cd5a6548336
ndragun92 <nemanjadragun92@gmail.com>
2025-03-04 22:36:23 +0100

    refactor: update main app to Playground with UI enhancements
    Replaced `App.vue` with `Playground.vue` as the main app component. Updated the UI with enhanced styling and added new icons (`IconPlay` and `IconPause`) for better user experience. Streamlined the track list presentation and improved layout classes for cleaner visuals.

--- src/Playground.vue
+++ src/Playground.vue
@@ -1,37 +1,37 @@
 <template>
   <main>
-    <h1>Tracks</h1>
+    <h1 class="bg-neutral-950 p-4 text-3xl">Playground</h1>
     <div>
-      <button type="button" @click="onPlayAsPlaylist(tracks)">Play all tracks</button>
-    </div>
-    <div>
-      <ul>
-        <li v-for="track in tracks" :key="track.id">
+      <ul class="space-y-4 bg-neutral-800 p-8">
+        <li class="mb-6 font-semibold text-orange-400">Play single track example</li>
+        <li v-for="track in tracks" :key="track.id" class="flex items-center gap-4">
           <button
-            class="cursor-pointer hover:text-red-500"
+            class="flex cursor-pointer items-center justify-center hover:text-red-500"
             type="button"
             @click="onPlaySingleTrack(track)"
           >
-            {{ isTrackPlaying(track.id) ? 'Pause' : 'Play' }}
+            <IconPlay class="size-6" v-if="!isTrackPlaying(track.id)" />
+            <IconPause class="size-6" v-else />
           </button>
-          {{ track.title }}
+          <span>{{ track.title }}</span>
         </li>
       </ul>
-      <hr />
-      <ul>
-        <li v-for="track in tracks" :key="track.id">
+      <hr class="border-neutral-600" />
+      <ul class="space-y-4 bg-neutral-800 p-8">
+        <li class="mb-6 font-semibold text-orange-400">Play track as playlist example</li>
+        <li v-for="track in tracks" :key="track.id" class="flex items-center gap-4">
           <button
-            class="cursor-pointer hover:text-red-500"
+            class="flex cursor-pointer items-center justify-center hover:text-red-500"
             type="button"
             @click="onPlayAsPlaylist(tracks, track)"
           >
-            {{ isTrackPlaying(track.id) ? 'Pause' : 'Play' }}
+            <IconPlay class="size-6" v-if="!isTrackPlaying(track.id)" />
+            <IconPause class="size-6" v-else />
           </button>
-          {{ track.title }}
+          <span>{{ track.title }}</span>
         </li>
       </ul>
     </div>
-    <hr />
     <AudioPlayer
       :options="{
         // waveColor: '#ffffff',
@@ -59,6 +59,8 @@ import AudioPlayer from './components/AudioPlayer.vue'
 import usePlayer, { type TPlayerTrack } from './composables/usePlayer'
 import { ref } from 'vue'
 import IconHeart from './components/Icons/IconHeart.vue'
+import IconPlay from './components/Icons/IconPlay.vue'
+import IconPause from './components/Icons/IconPause.vue'
 
 const { isTrackPlaying, onPlaySingleTrack, onPlayAsPlaylist } = usePlayer()
 
--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,6 @@
 import './assets/main.css'
 
 import { createApp } from 'vue'
-import App from './App.vue'
+import App from './Playground.vue'
 
 createApp(App).mount('#app')
